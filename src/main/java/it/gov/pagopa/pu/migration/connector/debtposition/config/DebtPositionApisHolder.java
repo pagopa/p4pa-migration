cffb9e6e9ce53ff36e1b19297d86a2d35c735c30
dgiacobbe <domenico.giacobbe@eng.it>
2025-05-30 13:06:34 +0200

    P4ADEV-3023 code review

--- src/main/java/it/gov/pagopa/pu/migration/connector/debtposition/config/DebtPositionApisHolder.java
+++ src/main/java/it/gov/pagopa/pu/migration/connector/debtposition/config/DebtPositionApisHolder.java
@@ -1,11 +1,12 @@
 package it.gov.pagopa.pu.migration.connector.debtposition.config;
 
-import it.gov.pagopa.pu.debtposition.client.generated.*;
+import it.gov.pagopa.pu.debtposition.client.generated.DebtPositionTypeOrgApi;
+import it.gov.pagopa.pu.debtposition.client.generated.DebtPositionTypeOrgEntityControllerApi;
+import it.gov.pagopa.pu.debtposition.client.generated.DebtPositionTypeOrgSearchControllerApi;
 import it.gov.pagopa.pu.debtposition.generated.ApiClient;
 import it.gov.pagopa.pu.debtposition.generated.BaseApi;
 import it.gov.pagopa.pu.migration.config.rest.RestTemplateConfig;
 import jakarta.annotation.PreDestroy;
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,8 @@ public class DebtPositionApisHolder {
     private final DebtPositionTypeOrgEntityControllerApi debtPositionTypeOrgEntityApi;
     private final DebtPositionTypeOrgSearchControllerApi debtPositionTypeOrgSearchControllerApi;
 
-    /** it will store the actual accessToken and mappedExternalUserId */
-    private final ThreadLocal<Pair<String, String>> authContextHolder = new ThreadLocal<>();
+    /** it will store the actual accessToken */
+    private final ThreadLocal<String> authContextHolder = new ThreadLocal<>();
 
     public DebtPositionApisHolder(
             DebtPositionApiClientConfig clientConfig,
@@ -28,7 +29,7 @@ public class DebtPositionApisHolder {
         RestTemplate restTemplate = restTemplateBuilder.build();
         ApiClient apiClient = new ApiClient(restTemplate);
         apiClient.setBasePath(clientConfig.getBaseUrl());
-        apiClient.setBearerToken(() -> authContextHolder.get().getKey());
+        apiClient.setBearerToken(authContextHolder::get);
         apiClient.setMaxAttemptsForRetry(Math.max(1, clientConfig.getMaxAttempts()));
         apiClient.setWaitTimeMillis(clientConfig.getWaitTimeMillis());
         if (clientConfig.isPrintBodyWhenError()) {
@@ -47,19 +48,19 @@ public class DebtPositionApisHolder {
 
 
     public DebtPositionTypeOrgApi getDebtPositionTypeOrgApi(String accessToken) {
-        return getApi(accessToken, null, debtPositionTypeOrgApi);
+        return getApi(accessToken, debtPositionTypeOrgApi);
     }
 
     public DebtPositionTypeOrgEntityControllerApi getDebtPositionTypeOrgEntityApi(String accessToken) {
-        return getApi(accessToken, null, debtPositionTypeOrgEntityApi);
+        return getApi(accessToken,  debtPositionTypeOrgEntityApi);
     }
 
     public DebtPositionTypeOrgSearchControllerApi getDebtPositionTypeOrgSearchControllerApi(String accessToken) {
-        return getApi(accessToken, null, debtPositionTypeOrgSearchControllerApi);
+        return getApi(accessToken,  debtPositionTypeOrgSearchControllerApi);
     }
 
-    private <T extends BaseApi> T getApi(String accessToken, String mappedExternalUserId, T api) {
-        authContextHolder.set(Pair.of(accessToken, mappedExternalUserId));
+    private <T extends BaseApi> T getApi(String accessToken, T api) {
+        authContextHolder.set(accessToken);
         return api;
     }
 }
