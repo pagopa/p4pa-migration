17f106065e3a3232b39b3e970d3fd8d75e7117e0
sachianese <santolo.chianese@eng.it>
2025-06-05 16:49:46 +0200

    P4ADEV-3089 - PAYMENTS_REPORTING - Coverage reduction of duplications.

--- src/main/java/it/gov/pagopa/pu/migration/wf/activity/ingestion/organizations/OrganizationsMigrationFileTypeHandlerActivityImpl.java
+++ src/main/java/it/gov/pagopa/pu/migration/wf/activity/ingestion/organizations/OrganizationsMigrationFileTypeHandlerActivityImpl.java
@@ -10,22 +10,15 @@ import it.gov.pagopa.pu.migration.model.Uploads;
 import it.gov.pagopa.pu.migration.repository.UploadsRepository;
 import it.gov.pagopa.pu.migration.service.file.FileArchiverService;
 import it.gov.pagopa.pu.migration.service.file.ZipFileService;
-import it.gov.pagopa.pu.migration.utils.Utilities;
 import it.gov.pagopa.pu.migration.wf.activity.ingestion.BaseMigrationFileTypeHandlerActivity;
 import it.gov.pagopa.pu.migration.wf.dto.MigrationFileResult;
-import it.gov.pagopa.pu.migration.wf.exception.InvalidMigrationFileException;
 import it.gov.pagopa.pu.migration.wf.service.ingestion.MigrationFileRetrieverService;
 import it.gov.pagopa.pu.migration.wf.utils.WfConstants;
-import it.gov.pagopa.pu.organization.dto.generated.Organization;
 import it.gov.pagopa.pu.p4paprocessexecutions.dto.generated.IngestionFlowFile;
-import it.gov.pagopa.pu.p4paprocessexecutions.dto.generated.IngestionFlowFileStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -33,21 +26,15 @@ import java.util.List;
 @Slf4j
 public class OrganizationsMigrationFileTypeHandlerActivityImpl extends BaseMigrationFileTypeHandlerActivity<MigrationFileResult> implements OrganizationsMigrationFileTypeHandlerActivity {
 
-  private final FileShareService fileShareService;
-  private final AuthnService authnService;
-  private final OrganizationSearchClient organizationSearchClient;
-  private final ZipFileService zipFileService;
-
   public OrganizationsMigrationFileTypeHandlerActivityImpl(
     UploadsRepository uploadsRepository,
     MigrationFileRetrieverService fileRetrieverService,
     FileArchiverService fileArchiverService,
-    FileShareService fileShareService, AuthnService authnService, OrganizationSearchClient organizationSearchClient, ZipFileService zipFileService) {
-    super(uploadsRepository, fileRetrieverService, fileArchiverService);
-    this.fileShareService = fileShareService;
-    this.authnService = authnService;
-    this.organizationSearchClient = organizationSearchClient;
-    this.zipFileService = zipFileService;
+    FileShareService fileShareService,
+    AuthnService authnService,
+    OrganizationSearchClient organizationSearchClient,
+    ZipFileService zipFileService) {
+    super(uploadsRepository, fileRetrieverService, fileArchiverService, fileShareService, authnService, organizationSearchClient, zipFileService);
   }
 
   @Override
@@ -57,46 +44,11 @@ public class OrganizationsMigrationFileTypeHandlerActivityImpl extends BaseMigra
 
   @Override
   protected MigrationFileResult handleRetrievedFiles(List<Path> retrievedFiles, Uploads upload) {
-    if (retrievedFiles == null || retrievedFiles.isEmpty()) {
-      throw new InvalidMigrationFileException("No file found in the uploaded archive");
-    }
-
-    Organization organization = organizationSearchClient.getByOrganizationId(
-      upload.getOrganizationId(),
-      authnService.getAccessToken()
+    return handleFilesUpload(
+      retrievedFiles,
+      upload,
+      IngestionFlowFileType.ORGANIZATIONS,
+      IngestionFlowFile.IngestionFlowFileTypeEnum.ORGANIZATIONS
     );
-
-    List<IngestionFlowFile> filesUploaded = new ArrayList<>(retrievedFiles.size());
-    for (Path file : retrievedFiles) {
-      String fileName = file.getFileName().toString();
-      String zipName = Utilities.replaceFileExtension(fileName, ".zip");
-      Path zipFilePath = file.getParent().resolve(zipName);
-      File zippedFile = zipFileService.zipper(zipFilePath, List.of(file));
-
-      log.info("Processing unzipped file: {}", file);
-      Long id = fileShareService.uploadIngestionFlowFile(
-        upload.getOrganizationId(),
-        IngestionFlowFileType.ORGANIZATIONS,
-        new FileSystemResource(zippedFile),
-        authnService.getAccessToken(organization.getIpaCode())
-      );
-      filesUploaded.add(IngestionFlowFile.builder()
-        .ingestionFlowFileId(id)
-        .fileName(file.getFileName().toString())
-        .fileSize(file.toFile().length())
-        .ingestionFlowFileType(IngestionFlowFile.IngestionFlowFileTypeEnum.ORGANIZATIONS)
-        .organizationId(upload.getOrganizationId())
-        .operatorExternalId(upload.getUpdateOperatorExternalId())
-        .filePathName(file.getFileName().toString())
-        .status(IngestionFlowFileStatus.UPLOADED)
-        .fileOrigin("MIGRATION")
-        .build());
-    }
-    return MigrationFileResult.builder()
-      .fileSize(upload.getFileSize())
-      .numTotalFiles(retrievedFiles.size())
-      .numCorrectlyProcessedFiles(filesUploaded.size())
-      .ingestionFlowFiles(filesUploaded)
-      .build();
   }
 }
--- src/main/java/it/gov/pagopa/pu/migration/wf/activity/ingestion/paymentsreporting/PaymentsReportingMigrationFileTypeHandlerActivityImpl.java
+++ src/main/java/it/gov/pagopa/pu/migration/wf/activity/ingestion/paymentsreporting/PaymentsReportingMigrationFileTypeHandlerActivityImpl.java
@@ -10,23 +10,15 @@ import it.gov.pagopa.pu.migration.model.Uploads;
 import it.gov.pagopa.pu.migration.repository.UploadsRepository;
 import it.gov.pagopa.pu.migration.service.file.FileArchiverService;
 import it.gov.pagopa.pu.migration.service.file.ZipFileService;
-import it.gov.pagopa.pu.migration.utils.Utilities;
 import it.gov.pagopa.pu.migration.wf.activity.ingestion.BaseMigrationFileTypeHandlerActivity;
-import it.gov.pagopa.pu.migration.wf.activity.ingestion.organizations.OrganizationsMigrationFileTypeHandlerActivity;
 import it.gov.pagopa.pu.migration.wf.dto.MigrationFileResult;
-import it.gov.pagopa.pu.migration.wf.exception.InvalidMigrationFileException;
 import it.gov.pagopa.pu.migration.wf.service.ingestion.MigrationFileRetrieverService;
 import it.gov.pagopa.pu.migration.wf.utils.WfConstants;
-import it.gov.pagopa.pu.organization.dto.generated.Organization;
 import it.gov.pagopa.pu.p4paprocessexecutions.dto.generated.IngestionFlowFile;
-import it.gov.pagopa.pu.p4paprocessexecutions.dto.generated.IngestionFlowFileStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -34,21 +26,15 @@ import java.util.List;
 @Slf4j
 public class PaymentsReportingMigrationFileTypeHandlerActivityImpl extends BaseMigrationFileTypeHandlerActivity<MigrationFileResult> implements PaymentsReportingMigrationFileTypeHandlerActivity {
 
-  private final FileShareService fileShareService;
-  private final AuthnService authnService;
-  private final OrganizationSearchClient organizationSearchClient;
-  private final ZipFileService zipFileService;
-
   public PaymentsReportingMigrationFileTypeHandlerActivityImpl(
     UploadsRepository uploadsRepository,
     MigrationFileRetrieverService fileRetrieverService,
     FileArchiverService fileArchiverService,
-    FileShareService fileShareService, AuthnService authnService, OrganizationSearchClient organizationSearchClient, ZipFileService zipFileService) {
-    super(uploadsRepository, fileRetrieverService, fileArchiverService);
-    this.fileShareService = fileShareService;
-    this.authnService = authnService;
-    this.organizationSearchClient = organizationSearchClient;
-    this.zipFileService = zipFileService;
+    FileShareService fileShareService,
+    AuthnService authnService,
+    OrganizationSearchClient organizationSearchClient,
+    ZipFileService zipFileService) {
+    super(uploadsRepository, fileRetrieverService, fileArchiverService, fileShareService, authnService, organizationSearchClient, zipFileService);
   }
 
   @Override
@@ -58,46 +44,11 @@ public class PaymentsReportingMigrationFileTypeHandlerActivityImpl extends BaseM
 
   @Override
   protected MigrationFileResult handleRetrievedFiles(List<Path> retrievedFiles, Uploads upload) {
-    if (retrievedFiles == null || retrievedFiles.isEmpty()) {
-      throw new InvalidMigrationFileException("No file found in the uploaded archive");
-    }
-
-    Organization organization = organizationSearchClient.getByOrganizationId(
-      upload.getOrganizationId(),
-      authnService.getAccessToken()
+    return handleFilesUpload(
+      retrievedFiles,
+      upload,
+      IngestionFlowFileType.PAYMENTS_REPORTING,
+      IngestionFlowFile.IngestionFlowFileTypeEnum.PAYMENTS_REPORTING
     );
-
-    List<IngestionFlowFile> filesUploaded = new ArrayList<>(retrievedFiles.size());
-    for (Path file : retrievedFiles) {
-      String fileName = file.getFileName().toString();
-      String zipName = Utilities.replaceFileExtension(fileName, ".zip");
-      Path zipFilePath = file.getParent().resolve(zipName);
-      File zippedFile = zipFileService.zipper(zipFilePath, List.of(file));
-
-      log.info("Processing unzipped file: {}", file);
-      Long id = fileShareService.uploadIngestionFlowFile(
-        upload.getOrganizationId(),
-        IngestionFlowFileType.PAYMENTS_REPORTING,
-        new FileSystemResource(zippedFile),
-        authnService.getAccessToken(organization.getIpaCode())
-      );
-      filesUploaded.add(IngestionFlowFile.builder()
-        .ingestionFlowFileId(id)
-        .fileName(file.getFileName().toString())
-        .fileSize(file.toFile().length())
-        .ingestionFlowFileType(IngestionFlowFile.IngestionFlowFileTypeEnum.PAYMENTS_REPORTING)
-        .organizationId(upload.getOrganizationId())
-        .operatorExternalId(upload.getUpdateOperatorExternalId())
-        .filePathName(file.getFileName().toString())
-        .status(IngestionFlowFileStatus.UPLOADED)
-        .fileOrigin("MIGRATION")
-        .build());
-    }
-    return MigrationFileResult.builder()
-      .fileSize(upload.getFileSize())
-      .numTotalFiles(retrievedFiles.size())
-      .numCorrectlyProcessedFiles(filesUploaded.size())
-      .ingestionFlowFiles(filesUploaded)
-      .build();
   }
 }
