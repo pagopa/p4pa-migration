31e89d135515f43e67ddb8758109ea7ccd1bbfe6
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-06-23 12:47:32 +0200

    fix: P4ADEV-3193 fix cve (#24)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ import java.util.Objects
 
 plugins {
   java
-  id("org.springframework.boot") version "3.5.0"
+  id("org.springframework.boot") version "3.5.3"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "6.1.0.5360"
+  id("org.sonarqube") version "6.2.0.5505"
   id("com.github.ben-manes.versions") version "0.52.0"
   id("org.openapi.generator") version "7.13.0"
-  id("org.ajoberstar.grgit") version "5.3.0"
+  id("org.ajoberstar.grgit") version "5.3.2"
   id("com.gorylenko.gradle-git-properties") version "2.5.0"
 }
 
@@ -40,7 +40,7 @@ val bouncycastleVersion = "1.81"
 val postgresJdbcVersion = "42.7.7"
 val podamVersion = "8.0.2.RELEASE"
 val temporalVersion = "1.29.0"
-val protobufJavaVersion = "4.30.2"
+val protobufJavaVersion = "4.31.1"
 val guavaVersion = "33.4.8-jre"
 val otelVersion = "1.43.0"
 val openCsvVersion = "5.11"
