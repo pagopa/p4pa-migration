31250d717ad55e4af8d8bb872855d5dcbdccbc21
domenicogi <domenico.giacobbe@eng.it>
2025-06-26 14:27:39 +0200

    fix: P4ADEV-3291 fix mapping (#28)

--- src/main/java/it/gov/pagopa/pu/migration/repository/DebtPositionTypeOrgOperatorsRepository.java
+++ src/main/java/it/gov/pagopa/pu/migration/repository/DebtPositionTypeOrgOperatorsRepository.java
@@ -2,17 +2,19 @@ package it.gov.pagopa.pu.migration.repository;
 
 import it.gov.pagopa.pu.migration.model.DebtPositionTypeOrgOperators;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface DebtPositionTypeOrgOperatorsRepository extends JpaRepository<DebtPositionTypeOrgOperators, Long> {
 
     /**
-     * Finds the first operator by its IPA code and debt position type organization code.
+     * Finds the first operator by organizationId, debt position type organization code, and operator's fiscal code hash.
      *
      * @param organizationId the organization id
      * @param debtPositionTypeOrgCode the code of the debt position type organization
+     * @param cfOperatorHash the hash of the operator's fiscal code
      * @return an Optional containing the operator if found, or empty if not found
      */
-    Optional<DebtPositionTypeOrgOperators> findFirstByOrganizationIdAndDebtPositionTypeOrgCode(Long organizationId, String debtPositionTypeOrgCode);
+    Optional<DebtPositionTypeOrgOperators> findFirstByOrganizationIdAndDebtPositionTypeOrgCodeAndCfOperatorHash(Long organizationId, String debtPositionTypeOrgCode, byte[] cfOperatorHash);
 
 }
