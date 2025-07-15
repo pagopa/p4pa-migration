988a979a6891816e380d3ccdc3dcc67b25dc5dfd
dgiacobbe <domenico.giacobbe@eng.it>
2025-07-15 23:21:50 +0200

    P4ADEV-0003 clean code and update javadoc

--- src/main/java/it/gov/pagopa/pu/migration/wf/activity/ingestion/debtpositions/DebtPositionsMigrationFileTypeHandlerActivity.java
+++ src/main/java/it/gov/pagopa/pu/migration/wf/activity/ingestion/debtpositions/DebtPositionsMigrationFileTypeHandlerActivity.java
@@ -2,21 +2,19 @@ package it.gov.pagopa.pu.migration.wf.activity.ingestion.debtpositions;
 
 import io.temporal.activity.ActivityInterface;
 import io.temporal.activity.ActivityMethod;
-import it.gov.pagopa.pu.fileshare.dto.generated.IngestionFlowFileType;
 import it.gov.pagopa.pu.migration.wf.activity.ingestion.MigrationFileTypeHandlerActivity;
 import it.gov.pagopa.pu.migration.wf.dto.MigrationFileResult;
-import org.springframework.core.io.Resource;
 
 /**
- * It will process the organization migration data related to the input <i>uploadId</i>:
+ * It will process the debt position migration data related to the input <i>uploadId</i>:
+ * <p>
+ * The process includes:
  * <ol>
- *   <li>TODO describe what it will be done on unzipped files, eg:</li>
- *   <li>It will check the existence of the required files</li>
- *   <li>For each, it will upload the file through {@link it.gov.pagopa.pu.migration.connector.fileshare.FileShareService#uploadIngestionFlowFile(Long, IngestionFlowFileType, Resource, String)}</li>
- *   <li>Finally it will return the {@link MigrationFileResult}</li>
+ *   <li>Checking the existence of required files in the unzipped content</li>
+ *   <li>Uploading each file using FileShareService</li>
+ *   <li>Returning the {@link MigrationFileResult} with the outcome of the operation</li>
  * </ol>
- *
- * @see it.gov.pagopa.pu.migration.wf.activity.ingestion.BaseMigrationFileTypeHandlerActivity
+ * The implementation is provided by {@link DebtPositionsMigrationFileTypeHandlerActivityImpl} and uses the base logic from {@link it.gov.pagopa.pu.migration.wf.activity.ingestion.BaseMigrationFileTypeHandlerActivity}.
  */
 @ActivityInterface
 public interface DebtPositionsMigrationFileTypeHandlerActivity extends MigrationFileTypeHandlerActivity {
