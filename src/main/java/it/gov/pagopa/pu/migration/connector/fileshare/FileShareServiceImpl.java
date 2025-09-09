9207976faecbf0b7d5c3058454e6a7eec57696f6
dgiacobbe <domenico.giacobbe@eng.it>
2025-09-09 10:11:26 +0200

    P4ADEV-0006 manage invalid file exception

--- src/main/java/it/gov/pagopa/pu/migration/connector/fileshare/FileShareServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/migration/connector/fileshare/FileShareServiceImpl.java
@@ -5,6 +5,7 @@ import it.gov.pagopa.pu.migration.connector.fileshare.client.FileShareClient;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Service
 public class FileShareServiceImpl implements FileShareService {
@@ -19,9 +20,12 @@ public class FileShareServiceImpl implements FileShareService {
   public Long uploadIngestionFlowFile(Long organizationId, IngestionFlowFileType ingestionFlowFileType, Resource file, String accessToken) {
     try {
       return client.uploadIngestionFlowFile(organizationId, ingestionFlowFileType, file, accessToken);
-    } catch (org.springframework.web.client.HttpClientErrorException e) {
-      if (e.getStatusCode() == HttpStatus.BAD_REQUEST && e.getResponseBodyAsString() != null && e.getResponseBodyAsString().contains("\"code\":\"INVALID_FILE\"")) {
-        throw new IllegalArgumentException("File name must contain a valid version: [1_0, 1_1, 1_2, 1_3]");
+    } catch (HttpClientErrorException e) {
+      if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
+        e.getResponseBodyAsString();
+        if (e.getResponseBodyAsString().contains("\"code\":\"INVALID_FILE\"")) {
+          throw new IllegalArgumentException("File name must contain a valid version: [1_0, 1_1, 1_2, 1_3]");
+        }
       }
       throw e;
     }
